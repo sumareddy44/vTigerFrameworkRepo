0126e84a95f4b92b127623b90ea92511e49c078a
sumareddyo <sumareddyo.obulareddygari@gmail.com>
2024-08-16 15:23:33 +0530

    commit

---
+++ vTiger/src/test/java/com/vtiger/org/CreateOrgWithTypeTest.java
@@ -0,0 +1,12 @@
+package com.vtiger.org;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgWithTypeTest {
+
+	@Test
+	public void test()
+	{
+		System.out.println("created org with type successfully");
+	}
+}
